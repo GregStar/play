2520fb42742f35ddead3f32200b5c27a82414654
ibi sum <ibisum@gmail.com>
2013-05-03 19:32:17 +0200

    on android, remove dumb keyboard at startup..

--- hanappe-samples/main.lua
+++ hanappe-samples/main.lua
@@ -2,6 +2,13 @@
 local modules = require "modules"
 local config = require "config"
 
+
+if MOAIKeyboardAndroid ~= nil then
+    print("On Android, hiding keyboard..")
+    MOAIKeyboardAndroid.hideKeyboard()
+end
+
+
 -- start and open
 Application:start(config)
 SceneManager:openScene(config.mainScene)
